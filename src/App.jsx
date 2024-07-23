33c5b339af4a46332602c9dad037bde6eb1101b4
Ashwoq_s_ <ashwoq00786@gmail.com>
2024-07-24 02:15:08 +0530

    1st-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Spline from "@splinetool/react-spline";
+
+function App() {
+  return (
+    <>
+      <Spline scene="https://prod.spline.design/K9nTokw7zzkHeGV4/scene.splinecode" />
+    </>
+  );
+}
+
+export default App;
